e890dc7c1a3333d1874a43a366105b1844a13d44
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-03-05 11:21:28 -0800

    IO python

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,8 +4,10 @@
 
 class Student:
     """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student"""
+
         self.first_name = first_name
          self.last_name = last_name
         self.age = age

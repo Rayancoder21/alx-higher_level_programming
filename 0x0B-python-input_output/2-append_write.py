e7531accbc295a1a4e96ec8734ccffbbfebd5614
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-03-05 01:51:46 -0800

    IO python

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Definition """
 def append_write(filename="", text=""):
-    """ """
+    """returns the number of characters added"""
     with open(filename, "a", encoding="UTF-8") as f:
         return f.write(text)

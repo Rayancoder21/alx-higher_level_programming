f29d95082d99630fa07e4211c2f43df9cafa9432
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-03-06 22:39:32 -0800

    almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle(Base):
     """ Initialize a new Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialize a new Rectangle"""
         self.width = width
         self.height = height
         self.x = x

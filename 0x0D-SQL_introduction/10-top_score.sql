940582124f3cd3244bd8e62a3e8432e5cbdd03f5
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-03-15 05:29:32 -0700

    sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the database.
-SELECT `name`, `score` FROM `second_table`  SHOW ORDER BY `score` DESC;
+SELECT `name`, `score` FROM `second_table` ORDER BY `score` DESC;

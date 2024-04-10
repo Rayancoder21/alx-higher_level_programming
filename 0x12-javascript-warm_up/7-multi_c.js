a4ecbca5440696f41f724a30aa62d33ebdc954ad
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-04-10 09:01:02 -0700

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) {
+if (isNaN(process.argv[2] || process.argv[2] === undefined)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {

5bfee80e9a9ac3fe57b4513e27ffa18b74eea636
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-05-22 18:17:28 -0700

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const id = process.argv[2];
+request.get(url + id, function (error, res, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
